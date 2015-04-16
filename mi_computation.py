86b9506cc0f7baf6afe96b394cea09b1b574a9a8
Stephen Brennan <smb196@case.edu>
2015-04-15 22:56:41 -0400

    Fixed some issues transitioning to storage module.

--- mi_computation.py
+++ mi_computation.py
@@ -23,23 +23,11 @@ class MiExperiment(Experiment):
     def __init__(self):
         global storage
         super().__init__()
-        # CSV files to append after every trial finishes, in case of problems.
-        self.csvee = 'data/ee.csv'
-        self.csvmm = 'data/mm.csv'
-        self.csvem = 'data/em.csv'
-
-        # Pickles to save
-        self.eepickle = 'data/ee.pickle'
-        self.empickle = 'data/em.pickle'
-        self.mepickle = 'data/me.pickle'
-        self.mmpickle = 'data/mm.pickle'
-
         # Open the data files.
         with open('data/expression.pickle', 'rb') as f:
             self.expression = pickle.load(f)
         with open('data/mutations.pickle', 'rb') as f:
             self.mutations = pickle.load(f)
-
         storage = Storage(self.expression.index)
 
         # Precompute the entropy for the two matrices.
@@ -47,21 +35,6 @@ class MiExperiment(Experiment):
                                                         domain=(0, 1, 2))
         self.mutation_entropy = mf.precompute_entropy(self.mutations,
                                                       domain=(0, 1))
-
-        # Dataframes for all the results.
-        self.eedf = pd.DataFrame(index=self.expression.columns,
-                                 columns=self.expression.columns,
-                                 dtype=float)
-        self.emdf = pd.DataFrame(index=self.expression.columns,
-                                 columns=self.expression.columns,
-                                 dtype=float)
-        self.medf = pd.DataFrame(index=self.expression.columns,
-                                 columns=self.expression.columns,
-                                 dtype=float)
-        self.mmdf = pd.DataFrame(index=self.expression.columns,
-                                 columns=self.expression.columns,
-                                 dtype=float)
-
         self._params['gene'] = list(self.expression.columns)
 
     def task(self, config):
--- storage.py
+++ storage.py
@@ -1,5 +1,7 @@
 """Storage of data."""
 
+import pickle
+
 import numpy as np
 import pandas as pd
 
@@ -15,6 +17,11 @@ class Storage:
         self._em = pd.DataFrame(index=genes, columns=genes, dtype=float)
         self._me = pd.DataFrame(index=genes, columns=genes, dtype=float)
         self._mm = pd.DataFrame(index=genes, columns=genes, dtype=float)
+
+        self.eepickle = 'data/ee.pickle'
+        self.empickle = 'data/em.pickle'
+        self.mepickle = 'data/me.pickle'
+        self.mmpickle = 'data/mm.pickle'
 
     def append(self, fn, series):
         gene = series.name
@@ -38,3 +45,13 @@ class Storage:
     def store_mm(self, series):
         self._mm[series.name] = series
         self.append(_mmcsv, series)
+
+    def save(self):
+        with open(self.eepickle, 'wb') as f:
+            pickle.dump(self._ee, f)
+        with open(self.empickle, 'wb') as f:
+            pickle.dump(self._em, f)
+        with open(self.mepickle, 'wb') as f:
+            pickle.dump(self._me, f)
+        with open(self.mmpickle, 'wb') as f:
+            pickle.dump(self._mm, f)
