913327ad0822c68a6fca8989743ac74259f63f73
Stephen Brennan <smb196@case.edu>
2015-04-28 15:32:55 -0400

    Added a sort file that should fix the mess that ptipython left me.

---
+++ sort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Sorts CSVs produced by the experiment."""
+
+import pandas as pd
+
+
+def sort(fname, outcsv, outdf):
+
+    print('Reading CSV')
+    series = pd.read_csv(fname, index_col=[0, 1], sequeeze=True,
+                         skipinitialspace=True, header=None)
+
+    print('Sorting Series')
+    series.sort()  # in-place
+
+    print('Saving Series as CSV')
+    series.to_csv(outcsv)
+
+    print('Unstacking Series')
+    df = series.unstack()
+
+    print('Transposing DataFrame')
+    df = df.transpose()
+
+    print('Saving DataFrame')
+    df.to_pickle(outdf)
+
+
+if __name__ == '__main__':
+    import sys
+    sort(*sys.argv[1:])
